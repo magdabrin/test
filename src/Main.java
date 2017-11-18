965e4f53203993d84318ff362e9ca048a34c8e97
magdabrin <magdabrin@wp.pl>
2017-11-18 12:19:19 +0100

    My second commit!

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Madzia");
+        System.out.println("Piotrek");
         System.out.println("Gosia");
     }
 }
